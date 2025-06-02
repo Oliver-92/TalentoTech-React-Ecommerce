02cc5c415419136f349280ba1c42e04dfc93d658
Ezequiel Oliver <ezequiel.oliver@live.com.ar>
2025-06-02 18:20:13 -0300

    firebase correction

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCOiTJAtfkY3Zf11ctN2o3Ab5xlK20TGCg",
-    authDomain: "react-ecommerce-a08c8.firebaseapp.com",
-    projectId: "react-ecommerce-a08c8",
-    storageBucket: "react-ecommerce-a08c8.firebasestorage.app",
-    messagingSenderId: "965698846150",
-    appId: "1:965698846150:web:7ae554928ee8897c0846c7",
-    measurementId: "G-WES5C3K0ER"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
